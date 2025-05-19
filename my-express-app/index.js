945af08be8f2b90e4a59c0c97165f57c836e83e7
kikulabo <kikuchi@x-tech5.co.jp>
2025-05-19 23:27:11 +0900

    feat:  意図的にエラーを発生させるパスを追加

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -8,6 +8,17 @@ app.get('/', (_req, res) => {
   res.send('Hello from Node.js with Express on Sakura Cloud!\n');
 });
 
+// 意図的にエラーを発生させるパスを追加
+app.get('/testerror', (req, res) => {
+  try {
+    throw new Error('This is a test error for Mackerel log monitoring!');
+  } catch (error) {
+    console.error(`Error on /testerror path: ${error.message}`);
+    console.error(error.stack);
+    res.status(500).send('An intentional error occurred on the server.');
+  }
+});
+
 app.listen(port, hostname, () => {
   console.log(`Server running with Express at http://${hostname}:${port}/`);
 });
